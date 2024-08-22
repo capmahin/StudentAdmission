8cba75ae5164db43d15bba706975ddd38c704a0d
capmahin <mahinulhuque1996@gmail.com>
2024-08-22 15:58:07 +0600

    add

--- components/AdmissionForm.jsx
+++ components/AdmissionForm.jsx
@@ -3,8 +3,21 @@ export default function AdmissionForm(){
        <>
        <form>
              <div>
-                
+                <label htmlFor="fullname">Full Name</label>
+                <input type="text" id="fullname" placeholder="Student name" />
              </div>
+             <div>
+                <label htmlFor="email">Email</label>
+                <input type="text"  id="email" placeholder="@gmail.com"/>
+             </div>
+
+             <div>
+                <label htmlFor="Message"></label>
+                <textarea  id="message" placeholder="Type your message">
+
+                </textarea>
+             </div>
+             <button type="submit">Send</button>
        </form>
        </>
     )
