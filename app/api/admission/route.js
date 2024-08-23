ae08fc4d22dacf99accda5438ad4175c29d33608
capmahin <mahinulhuque1996@gmail.com>
2024-08-23 15:09:43 +0600

    working on route

--- app/api/admission/route.js
+++ app/api/admission/route.js
@@ -1,11 +1,22 @@
+import connectDB from "@/app/lib/mongodb";
+import Admission from "@/app/models/admission";
 import { NextResponse } from "next/server";
 
 export async function POST(req){
     const {fullname, email, message}= await req.json();
 
-    console.log("Full name:", fullname);
-     console.log("Email name:", email);
-     console.log("Message:", message);
+    try{
+        await connectDB();
+        await Admission.create({fullname,email,message});
+       
+        return NextResponse.json({
+            msg:["Message sent successfully"],
+            success:true,
+        })
+        
+    }catch(error){
+        
+    }
 
-     return NextResponse.json({msg:["hi from admission route.js"]})
+     
 }
