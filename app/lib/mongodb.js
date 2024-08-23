f313c0c05fa6d0a45d54295dd6336f46968e69f5
capmahin <mahinulhuque1996@gmail.com>
2024-08-23 11:39:54 +0600

    working in mongodbv

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,5 +1,13 @@
-const connectDB = async ()=>{
+import mongoose from "mongoose";
 
+const connectDB = async ()=>{
+  try {
+    if(mongoose.connection.readyState === 0){
+        
+    }
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 export default connectDB;
